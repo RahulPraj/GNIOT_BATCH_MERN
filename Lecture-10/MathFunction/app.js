5452d902014162cf8df27097f970271d056754df
RahulPraj <rahul971801@gmail.com>
2024-11-27 16:50:08 +0530

    js start

---
+++ Lecture-10/MathFunction/app.js
@@ -0,0 +1,58 @@
+
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.LN2);
+// console.log(Math.sqrt(25));
+// console.log(Math.min(10, 30, 5, 15, 1));
+// console.log(Math.max(10, 30, 5, 15, 1));
+// console.log(Math.round(10.2));
+// console.log(Math.round(10.6));
+// console.log(Math.ceil(10.000001));
+// console.log(Math.floor(10.999999));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor((Math.random()*10)));
+// console.log(Math.floor((Math.random()*6 + 22)));
+
+// == vs ====
+1 == 1
+true
+1 === 1
+true
+1 == '1'
+true
+1 === '1'
+false
+1 == true
+true
+1 === true
+false
+'1' == true
+true
+'1' === true
+false
+
+
+// typeOf()
+ypeof(1)
+'number'
+typeof("rahul")
+'string'
+typeof(10.54)
+'number'
+typeof(0/10)
+'number'
+typeof(0/0)
+'number'
+0/0
+NaN
+typeof(NaN)
+'number'
+Infinity
+Infinity
+typeof(Infinity)
+       
+'number'
+typeof(undefined)
+'undefined'
---
+++ Lecture-10/basicOfJs/app.js
@@ -0,0 +1,40 @@
+
+
+// strings
+// let userName = 'rahul';
+// let userName = 'rahul\'s prajapati';
+// let userName = "rahul\'s prajapati";
+// console.log(userName);
+
+// ----------------------
+// let firstName = "rahul";
+// let lastName = "prajapti";
+// // let  space = " ";
+
+// // console.log(firstName+ space + lastName);
+// // console.log(firstName+ " " + lastName);
+// let fullName = `my name is  ${firstName} ${lastName} . how are you? `
+// console.log(fullName)
+
+// number
+let a = 10;
+let b = 11.2;
+let c = -23;
+let d = 1323454363;
+let e = 25
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// console.log(a + b);
+// console.log(b-a);
+// console.log(a/b);
+// console.log(a%e);
+
+// ---------------------------
+// max and min value
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
