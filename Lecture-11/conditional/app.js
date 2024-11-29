85968cfbdc04790ec547c42996bf6e18dd58f1f4
RahulPraj <rahul971801@gmail.com>
2024-11-29 06:46:08 +0530

    object

---
+++ Lecture-11/conditional/app.js
@@ -0,0 +1,42 @@
+// let score = 50
+
+// case-1
+// if(score > 40)
+// {
+//     console.log("pass");
+//     console.log("fail");
+// }
+
+// case-2
+// if(score > 40)
+//         console.log("pass");
+//         console.log("fail");
+    
+
+
+// 
+// let age = 17;
+
+// if(age <= 21 && age >= 18){
+//     console.log("allowed in party but only drink cold drink")
+// }
+// else if(age < 18){
+//     console.log("not allowed in party")
+// }
+// else{
+//     console.log("allowed in party and can drink mojito");
+// }
+
+
+// let age = 17;
+let age = parseInt(prompt("apni age btao"));
+
+if(age <= 21 && age >= 18){
+    console.log("allowed in party but only drink cold drink")
+}
+else if(age < 18){
+    console.log("not allowed in party")
+}
+else{
+    console.log("allowed in party and can drink mojito");
+}
---
+++ Lecture-11/function/app.js
@@ -0,0 +1,10 @@
+
+function sum(){
+
+    // console.log("hii i am using function");
+    return("hii i am using function")
+
+}
+
+let ans = sum();
+console.log(ans);
