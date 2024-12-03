33e30ea18a61bac0e57b6bd45e0d0cf3184c2aa2
RahulPraj <rahul971801@gmail.com>
2024-12-03 22:02:17 +0530

    HOF

--- Lecture-13/how-js-works/app.js
+++ Lecture-13/how-js-works/app.js
@@ -18,15 +18,16 @@
 // console.log(b);
 
 // ---------------
-var a = 10;
 
-function b(){
-    console.log("inside b");
+// var a = 10;
 
-}
+// function b(){
+//     console.log("inside b");
 
-b();
-console.log(a);
+// }
+
+// b();
+// console.log(a);
 
 // ------------------
 
@@ -43,3 +44,59 @@ console.log(a);
 
 // b();
 // console.log(a);
+
+// -----------
+
+// var a = 10;
+// let b= 20;
+
+// function c(){
+//     var d = "rahul";
+//     let e = true;
+//     console.log(d);
+//     console.log(e);
+// }
+
+// console.log(a);
+// c();
+// console.log(b);
+
+
+// ----var----
+// console.log(a);
+// b();
+
+// var a = 10;
+// function b(){
+//     console.log("inside b");
+// }
+
+
+// -----let and const----
+// console.log(a);
+// b();
+
+// let a = 10;
+// function b(){
+//     console.log("inside b");
+// }
+
+// -----------
+console.log(b);// undefined
+fun();
+
+let a = 10;
+var b = 20;
+
+console.log(a); //10
+
+function fun(){
+    console.log(c); //undefined
+    var c = 30;
+    var d = "rahul";
+    console.log(d); //rahul
+}
+
+console.log(a); //10
+console.log(b); //20
+
---
+++ Lecture-14/Scope/app.js
@@ -0,0 +1,67 @@
+
+// var a = 10;
+ 
+// function lol(){
+//     console.log(a);
+// }
+
+// console.log(a);
+// lol();
+
+//--------------
+
+
+// var a = 10;
+ 
+// function lol(){
+//     var a = "rahul"
+//     console.log(a);
+// }
+
+// lol();
+// console.log(a);
+
+
+// block scope
+
+// {
+//     console.log("hello");
+
+// }
+
+// -------------
+
+// let score = 80;
+
+// if(score >= 50){
+//     console.log("hello");
+//     score = 70;
+
+// }
+
+// console.log(score);
+
+
+// let score = 80;
+
+// if(score >= 50){
+//     console.log("hello");
+//     let score = 70;
+//     console.log(score);
+
+// }
+
+// console.log(score);
+
+
+// ---------------------
+var score = 80;
+
+if(score >= 50){
+    console.log("hello");
+    var score = 70;
+    console.log(score);
+
+}
+
+console.log(score);
